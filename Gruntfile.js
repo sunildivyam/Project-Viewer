3463d526c92631a64c942a1082db4ea5e6b4357f
Sunil Kumar <sunil.divyam@gmail.com>
2016-03-05 15:03:03 +0530

    Grunt | minify | CSS Minification task | updated index and grunt files and minified styles files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,9 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		scriptsRootPath: 'app/subprojects/etc',
+		stylesRootPath: 'app/less',
 		distScriptsPath: 'app/dist/scripts',
+		distStylesPath: 'app/dist/styles',
 		distReportsPath: 'app/dist/reports',
 		externalScriptsPath: 'app/bower_components',
 
@@ -69,17 +71,66 @@ module.exports = function(grunt) {
 					'<%=distScriptsPath%>/<%=pkg.name%>-externals.min.js': ['<%=concat.distExternalJS.dest%>']
 				}
 			}
+		},
+		less: {
+			dist: {
+				options: {
+					cleancss: true,
+					paths: [
+					'<%=stylesRootPath%>/mixins/_*.less',
+					'<%=stylesRootPath%>/_*.less'
+					]
+				},
+				files: {
+					'<%=distStylesPath%>/<%=pkg.name%>.css': '<%=stylesRootPath%>/<%=pkg.name%>.less'
+				}
+			}
+		},
+		cssmin: {
+			dist: {
+				options: {
+
+				},
+				files: {
+					'<%=distStylesPath%>/<%=pkg.name%>.min.css': '<%=distStylesPath%>/<%=pkg.name%>.css'
+				}
+			}
+		},
+		watch: {
+			options: {
+				interrupt: true
+			},
+			css: {
+				files: [
+					'<%=stylesRootPath%>/*/*.less'
+				],
+				tasks: ['minifystyles']
+			},
+			projectJS: {
+				files: [
+					'<%=scriptsRootPath%>/*/*.js',
+					'<%=scriptsRootPath%>/*/*/*.js'
+				],
+				tasks: ['minifyprojectjs']
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'minifystyles']);
+
 	grunt.registerTask('test', ['jshint']);
+
 	grunt.registerTask('minifyexternaljs', ['concat:distExternalJS','uglify:distExternalJS']);
 	grunt.registerTask('minifyprojectjs', ['concat:distProjectJS','uglify:distProjectJS']);
-	grunt.registerTask('minify', ['concat','uglify']);
 
+	grunt.registerTask('minifystyles', ['less', 'cssmin']);
+
+	grunt.registerTask('minify', ['concat','uglify', 'minifystyles']);
 };
